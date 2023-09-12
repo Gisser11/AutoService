527ed0777ba9bed5fcabaa0ed06a01e44bdb960b
Gisser11 <artem-sergeev-02@mail.ru>
2023-09-12 15:24:08 +0300

    Add React and .net env

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace backendSevice.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new[]
+            {
+                new { Name = "oleg"},
+                new { Name = "ildar"},
+            };
+            return Ok(users);
+        }
+
+    }
+}
+
